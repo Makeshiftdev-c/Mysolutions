c6ae4b8d938dbdb1a07fd4ac47944e279ac5dd7a
Makeshiftdev-c <abde_321@hotmail.com>
2019-11-14 02:51:49 +0000

    line source file

---
+++ CR15PP01/line.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <string.h>
+#include "line.h"
+
+#define MAX_LINE_LEN 60
+
+char line[MAX_LINE_LEN + 1];
+int line_len = 0;
+int num_words = 0;
+
+void clear_line(void)
+{
+	line[0] = '\0';
+	line_len = 0;
+	num_words = 0;
+}
+
+void add_word(const char *word)
+{
+	if (num_words > 0)
+	{
+		line[line_len] = ' ';
+		line[line_len + 1] = '\0';
+		line_len++;
+	}
+	strcat(line, word);
+	line_len += strlen(word);
+	num_words++;
+}
+
+int space_remaining(void)
+{
+	return MAX_LINE_LEN - line_len;
+}
+
+void write_line(void)
+{
+	int extra_spaces, i, j;
+	int half_extra_spaces, shift;
+	
+	extra_spaces = MAX_LINE_LEN - line_len;
+	half_extra_spaces = extra_spaces / 2;
+	shift = num_words - extra_spaces;
+	for (i = 0; i < line_len; i++)
+	{
+		if (line[i] != ' ')
+		{
+			putchar(line[i]);
+		}
+		else
+		{
+			putchar(' ');
+			if (num_words - shift == extra_spaces)
+			{
+				if (extra_spaces > half_extra_spaces)
+				{
+					putchar(' ');
+					extra_spaces--;
+				}
+			}
+			else if ((num_words - 1) == extra_spaces)
+			{
+				putchar(' ');
+				extra_spaces--;
+			}
+			
+			num_words--;
+		}
+	}
+	putchar('\n');
+}
+
+void flush_line(void)
+{
+	if (line_len > 0)
+	{
+		puts(line);
+	}
+}
