d8da5d60d2e09915f5c323ca592f3b0695cd5fb0
Makeshiftdev-c <abde_321@hotmail.com>
2019-11-27 00:09:15 +0000

    find_country function prototype

---
+++ CR16PP01/search.h
@@ -0,0 +1,14 @@
+#ifndef FIND_H
+#define FIND_H
+
+/************************************************************
+* find_country: Assings string of a countries name that     *
+*               corresponds to the country code, passed     *
+*               to the code parameter, to a character array.*
+*               array_length parameter prevents said        *
+*               assignment from going overbounds.           *
+*************************************************************/
+
+void find_country(char *array, int array_length, int code);
+
+#endif
