321f45d1d18a6750c69316f062590cd7aa0889f8
Makeshiftdev-c <abde_321@hotmail.com>
2019-12-19 02:32:56 +0000

    read_line function definition

---
+++ CR17PP07/readline.h
@@ -0,0 +1,13 @@
+#ifndef READLINE_H
+#define READLINE_H
+
+/************************************************************
+* read_line: Reads characters and stores them in str.       *
+*            Truncates the input if the number of           *
+*            characters exceeds n. Returns the number of    *
+*            characters stored.                             *
+*************************************************************/
+
+int read_line(char str[], int n);
+
+#endif
