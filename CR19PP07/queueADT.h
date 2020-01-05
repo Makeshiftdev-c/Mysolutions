e4729f92bd80567789fe086f06c259c908938d5c
Makeshiftdev-c <abde_321@hotmail.com>
2020-01-05 05:06:35 +0000

    Queue ADT Interface

---
+++ CR19PP07/queueADT.h
@@ -0,0 +1,18 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdbool.h>
+
+typedef int Item;
+
+typedef struct queue_type *Queue;
+
+Queue create(void);
+void destroy(Queue q);
+bool is_empty(Queue q);
+void enqueue(Queue q, Item i);
+Item dequeue(Queue q);
+Item peep_first(Queue q);
+Item peep_last(Queue q);
+
+#endif
