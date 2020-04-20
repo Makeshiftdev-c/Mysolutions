22ad3c8175b56a7a0629f6783abc953c27bd712d
Makeshiftdev-c <abde_321@hotmail.com>
2020-04-20 14:14:16 +0100

    Number Formatting Function Interface

---
+++ CR22PP17/format.h
@@ -0,0 +1,11 @@
+#ifndef FORMAT_H
+#define FORMAT_H
+
+/***********************************************************
+ * format: Modifies string of digits into '(xxx) xxx-xxxx' *
+ *         format; where x represents a digit (0-9).       *
+ ***********************************************************/
+
+void format(char *str);
+
+#endif
