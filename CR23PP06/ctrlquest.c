08783400966f039464d149532a7664d898bc1448
Makeshiftdev-c <abde_321@hotmail.com>
2020-05-27 04:29:54 +0100

    Chapter 23, Programming Project 6

---
+++ CR23PP06/ctrlquest.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(void)
+{
+	int ch;
+
+	while ((ch = getc(stdin)) != EOF)
+	{
+		if (iscntrl(ch) && ch != '\n')
+			ch = '?';
+		putc(ch, stdout);
+	}
+
+	return 0;
+}
